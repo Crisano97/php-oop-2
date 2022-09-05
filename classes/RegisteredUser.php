27cf5639eeb813e4533f8ef36acdb84467d96d42
Crisano97 <97keb6@gmail.com>
2022-09-05 17:05:01 +0200

    user class inheritance

---
+++ classes/RegisteredUser.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . "/User.php";
+class RegisteredUser extends User{
+    protected $username;
+    protected $password;
+    protected $discount;
+
+    public function __construct($_name, $_surname, $_dateOfBirth, $_email, $_address, $_methodOfPayment, $_isRegistered, $_username, $_password, $_discount){
+        parent::__construct($_name, $_surname, $_dateOfBirth, $_email, $_address, $_methodOfPayment, $_isRegistered);
+        $this->username = $_username;
+        $this->password = $_password;
+        $this->discount = $_discount;
+    }
+}
--- index.php
+++ index.php
@@ -3,11 +3,17 @@
     include_once __DIR__ . "/classes/Food.php";
     include_once __DIR__ . "/classes/Toy.php";
     include_once __DIR__ . "/classes/Gadget.php";
+    include_once __DIR__ . "/classes/User.php";
+    include_once __DIR__ . "/classes/RegisteredUser.php";
+
+
 
     $product = new Product("food", "food food food", 10);
     $food = new Food("bone","solid bone", 10, "dog", "nutritional");
     $toy = new Toy("bone","sqeezing bone", 10, "rubber", "bone");
     $gadget = new Gadget("bone","sqeezing bone", 30, "dog house", "plastic", "big");
+    $user = new User("pippo","poppi","12/02/2022","pippo.poppi@gmail.com","via disney 14","disneycard",true);
+    $registeredUser = new User("pippo","poppi","12/02/2022","pippo.poppi@gmail.com","via disney 14","disneycard",true,"pippopoppi", "1234321", 20);
 
 
 
@@ -15,6 +21,10 @@
     var_dump($food);
     var_dump($toy);
     var_dump($gadget);
+    var_dump($user);
+    var_dump($registeredUser);
+
+
 
 
 
@@ -35,9 +45,9 @@
         Food(petType, nutritionalValues)
         Toy(material, type)
         Gadget(type, material, petSize)
-        User(userNumber, address, email)
-        RegisteredUser(name, surname, username, dateOfBirth, password, registered)
-        GuestUser(unregistered)
+        User(userNumber, isRegistered)
+        RegisteredUser(name, surname, username, address, email, dateOfBirth, password)
+        GuestUser()
         MethodOfPayement(number, date)
      -->
 </body>
